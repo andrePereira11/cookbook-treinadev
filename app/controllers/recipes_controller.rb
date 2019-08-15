c2e1e590155f62860109720261cc695dfb52ce37
Andre <andreluis.alap@gmail.com>
2019-08-15 00:38:40 -0300

    Feature Editar realizada

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,16 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
+  end
+
   private
 
   def recipe_params
